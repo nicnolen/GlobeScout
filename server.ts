657150500dd5856e1a38f124002c9b6e211fceaf
Nic Nolen <nicnolen@Nics-MacBook-Pro.local>
2025-03-03 16:59:34 -0600

    refactored server.ts with improving typing as well as an async function to start the server

--- server.ts
+++ server.ts
@@ -1,23 +1,42 @@
 import express, { Express, Request, Response } from 'express';
 import next from 'next';
-import path from 'path';
+import dotenv from 'dotenv';
 
-const PORT = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev, dir: path.resolve(__dirname, './') });
+// Load environmental variables
+dotenv.config();
+
+const PORT: string | number = process.env.PORT || 3000;
+const dev: boolean = process.env.NODE_ENV !== 'production';
+const app = next({ dev, dir: './' });
+// Tell Express how to handle incoming requests to server Next.js pages
 const handle = app.getRequestHandler();
 
-// Prepare and start the server
-app.prepare().then(() => {
-    const server: Express = express();
+async function startServer(): Promise<void> {
+    try {
+        // Wait for Next.js to be ready;
+        await app.prepare();
 
-    // Catch-all route to handle Next.js pages
-    server.get('*', (req: Request, res: Response) => {
-        return handle(req, res);
-    });
+        const server: Express = express();
 
-    server.listen(PORT, (err) => {
-        if (err) throw err;
-        console.log('Express Server running on http://localhost:3000');
-    });
-});
+        // Catch all route to handle Next.js pages
+        server.get('*', (req: Request, res: Response) => {
+            return handle(req, res);
+        });
+
+        server.listen(PORT, (err?: Error) => {
+            if (err) {
+                console.error(`Error starting server: ${err}`);
+            }
+
+            console.info(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            console.error(`Error starting server: ${err.message}`);
+        } else {
+            console.error(`Error starting server: ${err}`);
+        }
+    }
+}
+
+startServer();
