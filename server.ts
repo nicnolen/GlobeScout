1f31d12ba2e2ba7de005d2e9a10fc263ae87b6f7
Nicholas Nolen <nicnolen@ymail.com>
2025-03-03 18:47:57 -0600

    Czen 12 set up express with appropriate middleware (#4)
    * initial express and next integration setup
    
    * refactored server.ts with improving typing as well as an async function to start the server
    
    * moved all next folders and files to a client folder and updated .lintstagedrc
    
    ---------
    
    Co-authored-by: Nic Nolen <nicnolen@Nics-MacBook-Pro.local>

---
+++ server.ts
@@ -0,0 +1,46 @@
+import express, { Express, Request, Response } from 'express';
+import next from 'next';
+import path from 'path';
+import dotenv from 'dotenv';
+
+// Load environmental variables
+dotenv.config();
+
+const PORT: string | number = process.env.PORT || 3000;
+const dev: boolean = process.env.NODE_ENV !== 'production';
+const app = next({ dev, dir: './client' });
+// Tell Express how to handle incoming requests to server Next.js pages
+const handle = app.getRequestHandler();
+
+async function startServer(): Promise<void> {
+    try {
+        // Wait for Next.js to be ready;
+        await app.prepare();
+
+        const server: Express = express();
+
+        // Serve static files from the `client/public` folder
+        server.use(express.static(path.join(__dirname, 'client', 'public')));
+
+        // Catch all route to handle Next.js pages
+        server.get('*', (req: Request, res: Response) => {
+            return handle(req, res);
+        });
+
+        server.listen(PORT, (err?: Error) => {
+            if (err) {
+                console.error(`Error starting server: ${err}`);
+            }
+
+            console.info(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            console.error(`Error starting server: ${err.message}`);
+        } else {
+            console.error(`Error starting server: ${err}`);
+        }
+    }
+}
+
+startServer();
