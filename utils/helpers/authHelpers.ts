cec9eec97f0b9e9ebebb0ca60563fd496b7dfb3c
Nic Nolen <nicnolen@Nics-MacBook-Pro.local>
2025-04-23 13:09:48 -0500

    proxying through vercel

--- utils/helpers/authHelpers.ts
+++ utils/helpers/authHelpers.ts
@@ -1,14 +1,21 @@
 interface CookieOptions {
     httpOnly: boolean;
     secure: boolean;
-    sameSite: 'strict';
+    sameSite: 'none';
     maxAge: number;
+    domain: string;
 }
 export function cookieOptions(maxAge: number): CookieOptions {
+    const domain =
+        process.env.NODE_ENV === 'development'
+            ? 'localhost' // Use the Vercel domain for production
+            : 'globe-scout.vercel.app'; // Use localhost for development
+
     return {
         httpOnly: true,
         secure: true,
-        sameSite: 'strict',
+        sameSite: 'none',
         maxAge: maxAge,
+        domain: domain,
     };
 }
