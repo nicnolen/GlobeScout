d8b13bc02996865cadc4fc894d48deeb51e12e3d
Nicholas Nolen <nicnolen@ymail.com>
2025-04-23 16:01:00 -0500

    Fix cors issue (#38)
    * fixed issue where public routes werent redirected
    
    * got serverless offline working correctly
    
    ---------
    
    Co-authored-by: Nic Nolen <nicnolen@Nics-MacBook-Pro.local>

--- client/src/config/graphQL/apolloClient.ts
+++ client/src/config/graphQL/apolloClient.ts
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
     uri: `${process.env.NEXT_PUBLIC_URL_ENDPOINT}/graphql`,
+    credentials: 'include',
     cache: new InMemoryCache(),
 });
 
