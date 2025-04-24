fe9724fb2d5f2639603a0c1fdc2fa1f0d4fdccd0
Nic Nolen <nicnolen@Nics-MacBook-Pro.local>
2025-04-24 09:24:17 -0500

    created reverse proxy in next

--- client/next.config.ts
+++ client/next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: `${process.env.NEXT_PUBLIC_URL_ENDPOINT}`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
