f2df07a0cca75f366454edcbda389a2d0d54ddbd
Nic Nolen <nicnolen@Nics-MacBook-Pro.local>
2025-04-24 09:50:51 -0500

    changed api routes to be /api

--- client/src/utils/apiHandler.ts
+++ client/src/utils/apiHandler.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_URL_ENDPOINT,
+    baseURL: `${process.env.NEXT_PUBLIC_URL_ENDPOINT}/api`,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
